20eec28ef9f926f290e086380b29a46045d9680e
Dzhos_ds17 <Dzhos_ds17@74-16>
2024-02-28 13:36:27 +0200

    Add project files.

---
+++ DecodingThread/DecodingThread/DecodingThreadDlg.cpp
@@ -0,0 +1,99 @@
+#include "DecodingThreadDlg.h"
+
+DecodingThreadDlg* DecodingThreadDlg::ptr = NULL;
+static int created = 0;
+static int symbols = 0;
+static int totalsize = 0;
+DecodingThreadDlg::DecodingThreadDlg(void)
+{
+	ptr = this;
+}
+
+DecodingThreadDlg::~DecodingThreadDlg(void)
+{
+	
+}
+
+void DecodingThreadDlg::Cls_OnClose(HWND hwnd)
+{
+	EndDialog(hwnd, 0);
+}
+
+DWORD WINAPI Decoding_Thread(LPVOID lp)
+{
+	DecodingThreadDlg *ptr = (DecodingThreadDlg *)lp;
+	char buf[4096];
+	TCHAR str[MAX_PATH];
+	_tcscpy(str, GetCommandLine());
+	TCHAR seps[] = TEXT(" ");
+	TCHAR *token, *last;
+	token = _tcstok(str, seps); 
+	while(token != NULL)
+	{
+		token = _tcstok(NULL, seps);
+		if(token)
+			last = token;
+		created++;
+	}
+	wsprintf(str, TEXT("copymusic_%s.txt"), last);
+	HANDLE hSemaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, false, TEXT("{2525FD5F-12E6-47c0-838A-7C5CA1EBD169}"));
+	DWORD dwAnswer = WaitForSingleObject(hSemaphore, INFINITE);
+	if(dwAnswer == WAIT_OBJECT_0)
+	{
+		ifstream in(TEXT("coding.txt"), ios::binary | ios::in);
+		if(!in)
+		{
+			MessageBox(ptr->hDialog, TEXT("Ошибка открытия файла!"), TEXT("Семафор"), MB_OK | MB_ICONINFORMATION);
+			return 1;
+		}
+		ofstream out(str, ios::binary | ios::out | ios::trunc);
+		const int KEY = 100;
+		while(!in.eof())
+		{
+			in.read(buf, 4096);
+			int n = in.gcount();
+			symbols += n;
+			totalsize += n * sizeof(wchar_t);
+			for(int i = 0; i < n; i++)
+			{
+				buf[i] ^= KEY;
+			}
+			out.write(buf, n);
+		}
+		out.close();
+		in.close();
+		ReleaseSemaphore(hSemaphore, 1, NULL);
+		MessageBox(ptr->hDialog, TEXT("Чтение данных из файла coding.txt завершено!"), TEXT("Семафор"), MB_OK | MB_ICONINFORMATION);
+	}
+
+
+	ofstream sizeFile("size.txt");
+	sizeFile << totalsize;
+	sizeFile.close();
+
+	ofstream symbolsFile("symbols.txt");
+	symbolsFile << symbols;
+	symbolsFile.close();
+
+	ofstream createdFile("created.txt");
+	createdFile << created;
+	createdFile.close();
+	return 0;
+}
+
+BOOL DecodingThreadDlg::Cls_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam) 
+{
+	hDialog = hwnd;
+	CreateThread(NULL, 0, Decoding_Thread, this, 0, NULL); 
+	return TRUE;
+}
+
+BOOL CALLBACK DecodingThreadDlg::DlgProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch(message)
+	{
+		HANDLE_MSG(hwnd, WM_CLOSE, ptr->Cls_OnClose);
+		HANDLE_MSG(hwnd, WM_INITDIALOG, ptr->Cls_OnInitDialog);
+	}
+	return FALSE;
+}
---
+++ info/info/main.cpp
@@ -0,0 +1,100 @@
+#include "Windows.h"
+#include "tchar.h"
+#include "resource.h"
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+HWND hEdit1;
+HWND hEdit2;
+HWND hEdit3;
+HWND hEdit4;
+
+
+
+void UpdateEditText(HWND hEdit, const char* filename) { //функция, которая считывает из файла информацию и выводит в статики
+    FILE* file;
+    if (fopen_s(&file, filename, "r") != 0)
+    {
+        MessageBox(NULL, TEXT("Ошибка открытия файла!"), TEXT("Ошибка"), MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    const int bufferSize = 4096;
+    char buffer[bufferSize];
+
+    while (fscanf_s(file, "%4095[^\n]", buffer, bufferSize) == 1) //находим инфу
+    {
+        strcat_s(buffer, "\r\n");
+        SendMessageA(hEdit, WM_SETTEXT, 0, LPARAM(buffer)); //считываем в статик
+    }
+
+    fclose(file);
+}
+
+DWORD WINAPI INFO(LPVOID lp) {
+    HANDLE hSemaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, false, TEXT("{2525FD5F-12E6-47c0-838A-7C5CA1EBD169}")); //открываем семафор
+    DWORD dwAnswer = WaitForSingleObject(hSemaphore, INFINITE); //обьект ожидания синхронизации
+  
+        UpdateEditText(hEdit2, "created.txt");
+        UpdateEditText(hEdit3, "symbols.txt");
+        UpdateEditText(hEdit4, "size.txt");
+
+    
+    ReleaseSemaphore(hSemaphore, 1, NULL); //освобождаем семафор
+
+    return 1;
+}
+
+INT_PTR CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+    {
+        hEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
+        hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
+        hEdit3 = GetDlgItem(hwnd, IDC_EDIT3);
+        hEdit4 = GetDlgItem(hwnd, IDC_EDIT4);
+        //получаем время
+        SYSTEMTIME currentTime;
+        GetLocalTime(&currentTime);
+
+        int len = GetWindowTextLength(hEdit1) + 1;
+        wchar_t* buffer = new wchar_t[len];
+        GetWindowText(hEdit1, buffer, len);
+
+        wchar_t dateTime[256];
+        swprintf_s(dateTime, L"%s %04d-%02d-%02d %02d:%02d:%02d",
+            buffer, currentTime.wYear, currentTime.wMonth, currentTime.wDay,
+            currentTime.wHour, currentTime.wMinute, currentTime.wSecond);
+
+        SendMessage(hEdit1, WM_SETTEXT, 0, LPARAM(dateTime));
+
+        delete[] buffer;
+        CreateThread(NULL, 0, INFO, nullptr, 0, NULL); //запускаем поток
+
+      
+       
+    }
+    break;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK) {
+            EndDialog(hwnd, 0);
+        }
+
+        break;
+
+    case WM_CLOSE:
+        EndDialog(hwnd, 0);
+        return TRUE;
+
+    }
+    return FALSE;
+}
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DLGPROC(DlgProc));
+}
